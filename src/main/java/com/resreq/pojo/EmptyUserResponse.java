dc9c7d259b4d9c807ff2f09c318ae981f0e4ba95
ABHINAV KUMAR <56204649+abhi9avx@users.noreply.github.com>
2025-06-08 23:00:29 +0530

    Add EmptyUserResponse POJO and implement GetUserNotFoundTest for 404 response validation

---
+++ src/main/java/com/resreq/pojo/EmptyUserResponse.java
@@ -0,0 +1,6 @@
+package com.resreq.pojo;
+
+public class EmptyUserResponse {
+    // For empty response, you might not even need any fields
+    // But you can add fallback fields if API sends something in future
+}
